00d6ab7c9cd911b8694ce9acee935e0f9e6ba79d
Markus <hutnik.markus@uwlax.edu>
2019-11-17 19:01:04 -0600

    added ability to edit workouts

--- exercise_tracker/tracker/urls.py
+++ exercise_tracker/tracker/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='tracker-dashboard'),
     path('create/', views.workout_create, name='workout-create'),
-    path(r'create/entry/<int:id>', views.entry_create, name='entry-create'),
+    path('create/entry/<int:id>/', views.entry_create, name='entry-create'),
+    path('details/<int:id>/', views.workout_detail, name='workout-detail')
 ]
--- exercise_tracker/tracker/views.py
+++ exercise_tracker/tracker/views.py
@@ -7,6 +7,7 @@ from .forms import WorkoutCreateForm, EntryCreateForm
 from datetime import datetime
 from matplotlib import pyplot as plt
 from django.db.models import Sum
+from django.contrib.auth.decorators import login_required
 
 
 def get_graph_data(workouts):
@@ -23,7 +24,7 @@ def get_graph_data(workouts):
 
 def dashboard(request):
 	# only entries for current user should show
-	workouts = Workout.objects.filter(author_id = request.user.id).order_by('-id')
+	workouts = Workout.objects.filter(author_id = request.user.id).order_by('-workout_date', '-id')
 	entries = Entry.objects.filter(workout_id__in=workouts)
 
 	x, y = get_graph_data(workouts)
@@ -42,6 +43,7 @@ def dashboard(request):
 
 	return render(request, 'tracker/dashboard.html', context)
 
+@login_required
 def workout_create(request):
 	auth_id = request.session['_auth_user_id']
 
@@ -58,7 +60,15 @@ def workout_create(request):
 	}
 	return render(request, 'tracker/workout_new.html', context)
 
+@login_required
 def entry_create(request, id=None):
+
+	auth_id = int(request.session['_auth_user_id'])
+	workout = Workout.objects.filter(id=id).values().first()
+
+	if auth_id != workout['author_id']:
+		return render(request, 'tracker/access_denied.html')
+
 	if request.method == 'POST':
 		form = EntryCreateForm(request.POST)
 		if form.is_valid():
@@ -67,7 +77,7 @@ def entry_create(request, id=None):
 			entry.workout_id = id
 			entry.save()
 			if request.POST.get('save'):
-				return redirect('tracker-dashboard')
+				return redirect('workout-detail', id)
 
 	context = {
 		'id': id,
@@ -75,4 +85,27 @@ def entry_create(request, id=None):
 	}
 
 	return render(request, 'tracker/add_exercises.html', context)
+
+@login_required
+def workout_detail(request, id=None):
+	auth_id = int(request.session['_auth_user_id'])
+	workout = Workout.objects.filter(id=id).values().first()
+
+	if auth_id != workout['author_id']:
+		return render(request, 'tracker/access_denied.html')
+
+	title = workout['title']
+	time_in_minutes = workout['time_in_minutes']
+	date = workout['workout_date']
+	entries = Entry.objects.filter(workout_id=id).all()
+
+
+	context = {
+		'id': id,
+		'title': title,
+		'time_in_minutes': time_in_minutes,
+		'date': date,
+		'entries': entries,
+	}
+	return render(request, 'tracker/workout_detail.html', context)
 	
